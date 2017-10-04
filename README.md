e8d7d2361480fce9829d35ee188e1ddbf6cbad43
Barryrowe <barry.rowe@lge-ku.com>
2017-10-04 14:24:56 -0400

    Update script and support files for XCode 8.3.3
    Since XCode 8.3.3 the PackageApplication utility is no longer shipped
    witht he product. There are work arounds to this, but they aren't
    sustainable in the long run, and we needed to switch over to the latest
    supported approach using `exportArchive`.
    
    This adds example build.json and export-ipa.plist files to support the
    changes required in how new xcode projects are signed and archived in
    the recent versions.
    
    The README is updated to walk through finding and setting the placeholder
    values for TEAM ID and provisioning profile name. This step needs to be
    automated, and split out into another file, but I'm only one person, doing
    this as I find time and energy.

--- README.md
+++ README.md
@@ -1,13 +1,49 @@
-#Provides
- - Build, Sign, and Package iOS app without Opening XCode
+# Provides
+ - Script to Build, Sign, and Package iOS app without Opening XCode
  - Build and Package Android app from command line
  - Places output archives (.ipa and .apk) to specified directory
+ - Reads App Name and version from config.xml to properly name the output .ipa/.apk file(s)
 
-Usage:
- 1. Place copy of build.sh in root of your cordova project (sibling of www folder)
- 2. Open build.sh and edit the top section for your application and environment
- 3. Run build.sh 
+# Usage:
+ 1. Place copy of build.sh, build.json, and export-ipa.plist in root of your cordova project (sibling of www folder)
+
+ 2. Look up your TeamId, Provisioning Profile Name, and TeamName from your signing Provisioning Profile. You can run the command below:
+
+     ```bash
+     security cms -D -i "/path/to/your/TargetProfile.mobileprovision"
+     ```
+  Note the following values:
+
+  Key Name | Maps To Placeholder
+  |:-----:|:--------:|
+   TeamIdentifier | DEV_TEAM_ID 
+   TeamName | DEV_TEAM_NAME
+   Name | PROVISIONING_PROFILE_NAME
+   
+ 3. Open build.sh and edit the top section for your application and environment, replacing any placeholder values with the values found in step #2 or your own custom values where applicable.
+ 
+ 4. Open build.json and replace the placeholder values with appropriate values found in step #2
+    - You can also change the release configuration to use `app-store` or other package types, and add any additional build flags you might need
+
+ 5. Open export-ipa.plist and replace appropriate values from step #2 and makes sure the value for `<key>method</key>` matches what you have in `packageType` for the build.json.
+
+ 6. Run the script
+
 ```
   ./build.sh
 ```
+
+# Notes
+- The script will `dev` for the environment, but you can provide the env as a parameter like below, to have the output name the files appropriately. 
+
+    ```
+    ./build.sh prod
+    ```
+- The script renames the output archives to include the version and ENV in the output .ipa/.apk files
+
+- The appname and version are read from the config.xml. You may want to create `config.xml.dev`, `config.xml.test`, `config.xml.prod`, etc if you have multiple versions and need to swap the names/versions of the app. Then just `cp config.xml.${env} config.xml` before running the build script.
+
+    - This might be an additional automated feature in the future.
+
+
 
--- build.sh
+++ build.sh
@@ -1,43 +1,61 @@
 #!/bin/bash
 
-if [ -z $ENV ]
+if [ -z $1 ]
 then
    lowerEnv="dev"
 else
-   lowerEnv=`echo $ENV | tr '[A-Z]' '[a-z]'`
+   lowerEnv=`echo $1 | tr '[A-Z]' '[a-z]'`
 fi
 upperEnv=`echo $lowerEnv | tr '[a-z]' '[A-Z]'`
 
 now=`date +%Y-%m-%d-%H%M`
- 
+
 #Update Application Specific Values HERE
-version=0.00.00
-archivePath="${HOME}/Deployables/APPNAME"
-packagePrefix="app-name-${lowerEnv}"
+buildAndroid=false
 indexWaitTime=20
-appNamePrefix="APPNAME"
-if [ $upperEnv = "PROD" ]
-then
-	appName="${appNamePrefix}"
-else
-	appName="${appNamePrefix} ${upperEnv}"
-fi
+archivePath="${HOME}/Path/To/The/outputDirectory"
+packagePrefix="<DESIRED-OUTPUT-PREFIX>-${lowerEnv}"
+codeSignIdentity="iPhone Distribution"
+exportPlistLocation="export-ipa.plist";
+buildJsonFile="build.json"
+
+#Update with your Signing values here
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# To Find these values if you aren't sure of them you can run the 
+# following command on your target provisioning profile:
+#
+#   security cms -D -i "/path/to/your/TargetProfile.mobileprovision"
+#
+# in the output look for the Name and TeamIdentifier Keys
+#   <key>Name</key>
+#   <string>NAME OF YOUR PROVISIONING PROFILE IS HERE</string>
+#
+#   <key>TeamIdentifier</key>
+#		<array>
+#			<string>DEVELOPMENT TEAM ID IS HERE</string>
+#		</array>
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+provisioningProfileSpecifier="<PROVISIONING_PROFILE_NAME>"
+developmentTeamId="<DEV_TEAM_ID>"
+
 ##Uncomment the line below to override the default android build tools
 ## version used by cordova.
 #androidBuildToolVersion=22.0.1
-
-#Update with your Signing values here 
-provisioningProfile="XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX" 
-developmentTeamId="XXXXXXXXX"
-codeSignIdentity="iPhone Developer"
 
 #############################################################################################################
 #Start Generic Script
 #DO NOT MODIFY BELOW UNLESS ABSOLUTELY NECESSARY
 #############################################################################################################
 
+echo "-------Extracting Version, BundleId, and AppName from config.xml------"
+rawVersion=`cat "config.xml" | grep -e "<widget" | grep -o -e "version=\".*\" xmlns=" | grep -o -e "[0-9\.]"`
+version=`echo $rawVersion | sed -e 's/ //g'`
+bundleId=`cat config.xml | grep -e "<widget" | grep -o -E "id=\"(.*)\" ver" | sed -e 's/\" ver//g' | sed -e 's/id=\"//g'`
+appName=`cat config.xml | grep -e "<name>" | sed -e 's/<name>//g; s/<\/name>//g; s/^\s//g; s/^ *//;s/ *$//;'`
+
 echo "------CONFIG------"
 echo "version: ${version}"
+echo "bundleId: ${bundleId}"
 echo "archivePath: ${archivePath}"
 echo "appName: ${appName}"
 echo "packagePrefix: ${packagePrefix}"
@@ -45,7 +63,8 @@ echo "indexWaitTime: ${indexWaitTime}"
 echo "androidBuildToolVersion: ${androidBuildToolVersion}"
 
 archiveLocation="${archivePath}/${now}_${appName}.xcarchive"
-appLocation="${archiveLocation}/Products/Applications/${appName}.app"
+exportedIpaRoot="${archivePath}/${upperEnv}"
+outputIpaLocation="${archivePath}/${upperEnv}/${packagePrefix}-v${version}.ipa"
 ipaLocation="${archivePath}/${appName}.ipa"
 xcodeProjectLocation="platforms/ios/${appName}.xcodeproj"
 
@@ -54,7 +73,7 @@ cordova platform remove ios
 cordova platform add ios
 
 echo "------PRE-BUILDING iOS------"
-cordova build ios
+cordova build --release --buildConfig build.json ios
 
 open "${xcodeProjectLocation}" 
 echo "---------Waiting ${indexWaitTime} seconds to let project index---------"
@@ -64,32 +83,47 @@ osascript -e 'quit app "Xcode"'
 
 echo "---------Beginning xcodebuild---------"
 xcodebuild \
+	-sdk iphoneos \
 	-project "${xcodeProjectLocation}" \
-	-scheme "${appName}" archive \
+	-scheme "${appName}" \
+	-configuration Release build \
 	-archivePath "${archiveLocation}" \
-	-configuration Release \
-	DEVELOPMENT_TEAM="${developmentTeamId}" \
-	CODE_SIGN_IDENTITY="${codeSignIdentity}"
-	#PROVISIONING_PROFILE="${provisioningProfile}" \
+	archive \
+	DEVELOPMENT_TEAM="${devTeamId}" \
+	PROVISIONING_PROFILE_SPECIFIER="${provisioningProfileSpecifier}"
 
-xcrun \
-	-sdk iphoneos PackageApplication \
-	-v "${appLocation}" \
-	-o "${archivePath}/${packagePrefix}-v${version}.ipa"
+echo "------Export Archive Props------"
+echo "Archive Loction: ${archiveLocation}"
+echo "Export PLIST File: ${exportPlistLocation}"
+echo "Export Path: ${exportedIpaRoot}"
+xcodebuild \
+	-exportArchive \
+	-archivePath "${archiveLocation}" \
+	-exportPath "${exportedIpaRoot}" \
+	-exportOptionsPlist "${exportPlistLocation}" \
+
+echo "-------Renaming Exported IPA------"
+mv "${exportedIpaRoot}/${appName}.ipa" ${outputIpaLocation}
 
 #BUILDING ANDROID
-echo "------BUILDING ANDROID------"
-if [ -z "$androidBuildToolVersion" ]
+if [ "$buildAndroid" = true ]
 then
-        echo "---------No Android Build Tool Version set. Default to Cordova's Default"
-        cordova build android
-else
-        echo "---------Android Build Tool Version set. Using ${androidBuildToolVersion}"
-        cordova build android -- --gradleArg=-PcdvBuildToolsVersion=${androidBuildToolVersion}
+	echo "------RE-ADDING ANDROID PLATFORM------"
+	cordova platform remove android
+	cordova platform add android
+
+	echo "------BUILDING ANDROID------"
+	if [ -z "$androidBuildToolVersion" ]
+	then
+					echo "---------No Android Build Tool Version set. Default to Cordova's Default"
+					cordova build android
+	else
+					echo "---------Android Build Tool Version set. Using ${androidBuildToolVersion}"
+					cordova build android -- --gradleArg=-PcdvBuildToolsVersion=${androidBuildToolVersion}
+	fi
+
+	cp ./platforms/android/build/outputs/apk/android-debug.apk "${archivePath}/${packagePrefix}-v${version}.apk"
 fi
-
-cp ./platforms/android/build/outputs/apk/android-debug.apk "${archivePath}/${packagePrefix}-v${version}.apk"
-
 
 echo "------Archives Built-------"
 open ${archivePath}
